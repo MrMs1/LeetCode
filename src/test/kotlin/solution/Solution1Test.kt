6f5a003c4db348b1b72d6df16bf0e60a8c98a525
Masatomo Murata <masatomo.murata@justincase-tech.com>
2023-04-09 18:09:19 +0900

    disableテストの修正

--- src/test/kotlin/solution/Solution1Test.kt
+++ src/test/kotlin/solution/Solution1Test.kt
@@ -6,21 +6,19 @@ import org.junit.jupiter.api.Test
 
 class Solution1Test {
 
-    @Disabled
     @Test
     fun `0,4,3,0 0`() {
         val target = Solution1()
         val expected = intArrayOf(0, 3)
         val input = intArrayOf(0, 4, 3, 0)
-        assertEquals(expected, target.twoSum(input, 0))
+        assertArrayEquals(expected, target.twoSum(input, 0).sortedArray())
     }
 
-    @Disabled
     @Test
     fun `-3,4,3,90`() {
         val target = Solution1()
         val expected = intArrayOf(0, 2)
         val input = intArrayOf(-3, 4, 3, 90)
-        assertEquals(expected, target.twoSum(input, 0))
+        assertArrayEquals(expected, target.twoSum(input, 0).sortedArray())
     }
 }
